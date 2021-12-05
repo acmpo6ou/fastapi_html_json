4f9c245ddeceeceded4d878b0973f843522baf4d
acmpo6ou <acmpo6ou@gmail.com>
2021-12-05 17:09:55 +0200

    Implement add_request_param
    that automatically adds `request` parameter to view function's signature if it's not there already

--- html_json.py
+++ html_json.py
@@ -1,4 +1,5 @@
 from inspect import signature, Parameter, _ParameterKind
+from typing import Callable
 
 from fastapi import Request
 from functools import wraps
@@ -9,19 +10,35 @@ class HtmlJson:
     def __init__(self, templates_dir: str = "templates"):
         self.templates = Jinja2Templates(templates_dir)
 
-    def html_or_json(self, f):
+    @staticmethod
+    def add_request_param(wrapper: Callable, f: Callable):
+        sig = signature(f)
+        if 'request' in sig.parameters:
+            return
+
+        params = list(sig.parameters.values())
+        request_param = Parameter("request", _ParameterKind.POSITIONAL_OR_KEYWORD,
+                                  annotation=Request)
+        params.append(request_param)
+        wrapper.__signature__ = sig.replace(parameters=params)
+
+    def html_or_json(self, f: Callable):
         @wraps(f)
         async def wrapper(*args, **kwargs):
-            request = args[0]
-            result = await f()
-            accept = request.headers["accept"].split(",")[0]
+            request = kwargs['request']
 
+            try:
+                result = await f(*args, **kwargs)
+            except TypeError:
+                kwargs.pop('request')
+                result = await f(*args, **kwargs)
+
+            accept = request.headers["accept"].split(",")[0]
             if accept == "text/html":
                 result.update({"request": request})
                 return self.templates.TemplateResponse("index.html", result)
+
             return result
 
-        sig = signature(f)
-        request_param = Parameter("request", _ParameterKind.POSITIONAL_OR_KEYWORD,
-                                  annotation=Request)
+        self.add_request_param(wrapper, f)
         return wrapper
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from html_json import HtmlJson
 
 app = FastAPI()
@@ -7,5 +7,5 @@ hj = HtmlJson()
 
 @app.get("/")
 @hj.html_or_json
-async def root():
-    return {"message": "Hello World"}
+async def root(name: str):
+    return {"message": f"Hello {name}"}
