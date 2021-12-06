d429883f26abea3e7659479fd422e7cb579aaec5
acmpo6ou <acmpo6ou@gmail.com>
2021-12-06 08:55:23 +0200

    Test correct rendering of pydantic models and lists
    also test html_or_json template parameter

--- main.py
+++ main.py
@@ -2,16 +2,40 @@
 
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 from html_json import HtmlJson
 
 app = FastAPI(debug=True)
 hj = HtmlJson()
 
 
+class User(BaseModel):
+    name: str
+    id: int
+
+
 @app.get("/")
-@hj.html_or_json
+@hj.html_or_json('index.html')
 async def root(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/user")
+@hj.html_or_json('user.html')
+async def user():
+    return User(name="John", id=23)
+
+
+@app.get("/users")
+@hj.html_or_json('users.html')
+async def users():
+    return [
+        User(name="John", id=23),
+        User(name="Mike", id=98),
+        User(name="Timmy", id=72),
+    ]
+
 
 if __name__ == "__main__":
     uvicorn.run(app)
